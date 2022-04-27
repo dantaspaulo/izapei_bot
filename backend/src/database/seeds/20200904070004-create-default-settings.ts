518b0d88f70610c99d41837eaf04ef79ce40d9f9
OWENZAP <falecom@owenzap.com>
2022-04-27 16:31:44 -0300

    owenzapbot

---
+++ backend/src/database/seeds/20200904070004-create-default-settings.ts
@@ -0,0 +1,28 @@
+import { QueryInterface } from "sequelize";
+
+module.exports = {
+  up: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkInsert(
+      "Settings",
+      [
+        {
+          key: "userCreation",
+          value: "enabled",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+             key: "transferTicket",
+             value: "disabled",
+             createdAt: new Date(),
+             updatedAt: new Date()
+        }
+      ],
+      {}
+    );
+  },
+
+  down: (queryInterface: QueryInterface) => {
+    return queryInterface.bulkDelete("Settings", {});
+  }
+};
